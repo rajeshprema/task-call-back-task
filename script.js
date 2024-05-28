e2d95a3fe7a226ab69f2b85e825669e41dc69704
rajeshprema <122607002+rajeshprema@users.noreply.github.com>
2024-05-28 08:31:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+ 
+setTimeout(() => {
+  document.getElementById("main").innerHTML = "10";
+  setTimeout(() => {
+    document.getElementById("main").innerHTML = "9";
+    setTimeout(() => {
+      document.getElementById("main").innerHTML = "8";
+      setTimeout(() => {
+        document.getElementById("main").innerHTML = "7";
+        setTimeout(() => {
+          document.getElementById("main").innerHTML = "6";
+          setTimeout(() => {
+            document.getElementById("main").innerHTML = "5";
+            setTimeout(() => {
+              document.getElementById("main").innerHTML = "4";
+              setTimeout(() => {
+                document.getElementById("main").innerHTML = "3";
+                setTimeout(() => {
+                  document.getElementById("main").innerHTML = "2";
+                  setTimeout(() => {
+                    document.getElementById("main").innerHTML = "1";
+                    setTimeout(() => {
+                      document.getElementById("main").innerHTML =
+                        "Happy Independence Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+
+
+
+
+
+
+
+        
